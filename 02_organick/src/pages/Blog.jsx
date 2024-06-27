b234a8efd5eaa099e445d969710d10d96f183b5f
fullstackdev <rajenbalaji12@gmail.com>
2024-06-27 19:10:37 +0530

    Blog and Team page completed

--- 02_organick/src/pages/Blog.jsx
+++ 02_organick/src/pages/Blog.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import styles from "../style";
+import { useBlogs } from "../hooks";
+import { PageHeading, BlogCards, CTA } from "../components";
 const Blog = () => {
-  return <div>Blog</div>;
+  const blogs = useBlogs();
+  return (
+    <>
+      <PageHeading bgImage="bg_team" heading="Our Team" />
+      <div
+        className={`${styles.boxWidth} ${styles.padding} ${styles.flexCenter} flex-col`}
+      >
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-24 md:gap-8 mt-12">
+          {blogs.map((blog) => (
+            <BlogCards key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </div>
+      <CTA />
+    </>
+  );
 };
 export default Blog;
--- 02_organick/src/pages/Team.jsx
+++ 02_organick/src/pages/Team.jsx
@@ -1,5 +1,28 @@
 import React from "react";
-const Team = () => {
-  return <div>Team</div>;
-};
+import { experts } from "../constants";
+import styles from "../style";
+import { CTA, PageHeading, SectionHeading, TeamCard } from "../components";
+const Team = () => (
+  <>
+    <PageHeading bgImage="bg_team" heading="Our Team" />
+    <div
+      className={`${styles.boxWidth} ${styles.padding} ${styles.flexCenter} flex-col text-center`}
+    >
+      <div>
+        <SectionHeading title="Our Organic Experts" subtitle="team" />
+        <p className={`${styles.paragraph} max-w-[750px] mt-4`}>
+          Simply dummy text of the printing and typesetting industry. Lorem had
+          ceased to been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley.
+        </p>
+      </div>
+      <div className="w-full flex flex-wrap justify-center items-center gap-6 mt-12">
+        {experts.map((expert, index) => (
+          <TeamCard key={index} expert={expert} />
+        ))}
+      </div>
+    </div>
+    <CTA />
+  </>
+);
 export default Team;
